f5729a3dc085cf420d9f44602f10ff929791dba0
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-02-28 04:16:00 +0300

    add todoInput and render todoList on page

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,18 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from 'react-bootstrap/Button';
+import React, {useState, useEffect} from "react";
 
+import {Button,FormControl,Form} from 'react-bootstrap';
 
 
 function App() {
+
+  const [todo, setTodo] = useState("")
+  const [list,setList] = useState([])
+
+  const addToDo = () => {
+    setList([...list, todo])
+    setTodo("")
+  }
+
+  useEffect(() =>{
+    console.log(list)
+  },[list])
+
+
   return (
-    <div className="App">
-      
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <Button>Burak</Button>
+    <div className="text-center mt-5">
+      <h1 >To Do List</h1>
+      <div className='d-flex justify-content-center mt-4'>
+        <FormControl className='w-25 me-2'
+          placeholder="What needs to be done?"
+          value={todo}
+          onChange = {(e) => setTodo(e.target.value)}
+        />
+        <Button variant="outline-secondary" id="button-addon2" onClick={addToDo}>
+          Add To Do
+        </Button>
+      </div>
+      <div className="m-auto mt-5   w-50">
+        {
+          list.map((item,i) => <div key={i} className="d-flex justify-content-between ">
+          <div >
+            <label className="d-flex">
+                <Form.Check 
+                  type="checkbox"
+                  className="me-3"
+                />
+            {item}</label>
+            
+          </div>
+          <div>
+            <span>B1</span>
+            <span>B2</span>
+          </div>
+          
+        </div>)
+        }
+      </div>
+
     </div>
   );
 }
