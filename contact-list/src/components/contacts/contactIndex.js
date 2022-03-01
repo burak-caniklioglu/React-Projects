f8b81591c610ece2a5d47c22d0fa4f80efb4bfef
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-03-02 02:48:54 +0300

    Form is ready

--- contact-list/src/components/contacts/contactIndex.js
+++ contact-list/src/components/contacts/contactIndex.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Form from './Form/formIndex'
 import List from './List/listIndex'
 
 
 
 function Contact() {
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    console.log(contacts)
+  },[contacts])
+
   return (
     <div>
-      <Form />
+      <Form addContact ={setContacts} people={contacts}/>
       <List />
     </div>
   )
