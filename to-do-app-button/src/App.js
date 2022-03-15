a11aa96b6b182eb0c914e27ff19455cd3c0845aa
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-03-15 18:13:32 +0300

    Createarea is ready

--- to-do-app-button/src/App.js
+++ to-do-app-button/src/App.js
@@ -1,11 +1,22 @@
+import React, {useState} from "react"
 import Header from "./components/Header";
 import CreateArea from "./components/CreateArea";
 
 function App() {
+
+  const [todoList, setTodoList] = useState([])
+
+  function addNote(newNote){
+    setTodoList(prevTodoList => {
+      return [...prevTodoList, newNote]
+    })
+    
+  }
+  console.log(todoList)
   return (
     <div className="App">
       <Header />
-      <CreateArea />
+      <CreateArea addTodo = {addNote} />
     </div>
   );
 }
--- to-do-app-button/src/components/CreateArea.jsx
+++ to-do-app-button/src/components/CreateArea.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from '@mui/material/Fab';
 
-function CreateArea() {
+const initialValue = {title:"",
+content:""}
+
+function CreateArea({addTodo}) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setNote((prevNote) => {
+      return { ...prevNote, [name]: value };
+    });
+    
+  }
+
+  function handleSubmit(e) {
+    
+
+    addTodo(note)
+
+    setNote(initialValue)
+    e.preventDefault()
+    
+  }
+
   return (
     <div>
       <form action="" className="create-note">
-           
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={note.title}
+        />
+
+        <textarea
+          name="content"
+          rows="3"
+          placeholder="Take a note..."
+          onChange={handleChange}
+          value={note.content}
+        ></textarea>
+
+        <Fab color="primary" aria-label="add" onClick={handleSubmit}>
+          <AddIcon />
+        </Fab>
       </form>
     </div>
-  )
+  );
 }
 
-export default CreateArea
+export default CreateArea;
