0a169411f729c98311b71881035b9e187fa22a38
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-03-16 00:33:40 +0300

    Notes component added

--- to-do-app-button/src/App.js
+++ to-do-app-button/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react"
 import Header from "./components/Header";
 import CreateArea from "./components/CreateArea";
+import Notes from "./components/Notes";
 
 function App() {
 
@@ -17,6 +18,19 @@ function App() {
     <div className="App">
       <Header />
       <CreateArea addTodo = {addNote} />
+      {
+        todoList.map((item, index) => {
+          return ( 
+            <Notes 
+            key = {index}
+            id={index}
+            title={item.title}
+            content = {item.content}/>
+          )
+        })
+      }
+
+      
     </div>
   );
 }
