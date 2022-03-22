d736e2901a01db6620818e4cc81a66c95d9cd8dc
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-03-22 14:14:13 +0300

    First commit

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Home from './components/Main';
+import Data from './components/data';
+import { useState } from 'react';
+
+
+
+
+
+function App() {
+
+  const [liste,setListe] = useState(Data)
+
+
+
+  
+  return (
+    <div className="App" >
+      
+      <Home liste = {liste} setListe={setListe}/>
+    </div>
+  );
+}
+
+export default App;
