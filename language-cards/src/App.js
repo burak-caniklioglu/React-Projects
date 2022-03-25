bfecea41733937e229029364dcfadf830f7f0f5d
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-03-25 20:25:29 +0300

    Card on page

--- language-cards/src/App.js
+++ language-cards/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import { ReactComponent as ReactIcon } from "./assets/react.svg";
+import Card from './components/card/Card';
+import {categories} from "./helper/data"
+
 
 function App() {
   return (
     <div className="App">
-      
+      <ReactIcon className='appIcon'/>
+      <Card {...categories}/>
     </div>
   );
 }
