392ae53fbb88fd9274ed1d42fbed4a2fd8e88caa
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-04-03 03:00:35 +0300

    Navbar is ready

--- recipe-book/src/App.js
+++ recipe-book/src/App.js
@@ -1,10 +1,12 @@
-
+import Login from "./components/login/Login";
+import Navbar from "./components/navbar/Navbar";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Login />
     </div>
   );
 }
---
+++ recipe-book/src/components/login/Login.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+function Login() {
+  return (
+    <Container>
+        <Section>
+            <Image src='https://recipe-app-solution.vercel.app/static/media/meal.9eba888b37c920112c046969b9256a41.svg' />
+            <Header>
+            &#60;Burak&#62;"Recipe"
+            </Header>
+            <Form>
+                <Input type="text" placeholder='username' required/>
+                <Input type = "password" placeholder='password' required/>
+            </Form>
+            
+        </Section>
+    </Container>
+  )
+}
+
+
+const Container = styled.div`
+    background-image: url(https://picsum.photos/1600/900);
+    background-repeat: no-repeat;
+    height: 100vh;
+    background-size: cover;
+    background-position: center center;
+    display: flex;
+    -webkit-box-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    align-items: center;
+`
+
+const Section = styled.div`
+width: 600px;
+min-width: 600px;
+height: 600px;
+background-color: rgba(0, 173, 181, 0.5);
+border-radius: 50%;
+border: 2px solid rgb(225, 241, 221);
+padding: 5px;
+display: flex;
+-webkit-box-pack: center;
+justify-content: center;
+-webkit-box-align: center;
+align-items: center;
+flex-direction: column;
+`
+
+const Image = styled.img`
+    width: 150px;
+    margin: 1rem;
+`
+
+const Header = styled.h1`
+color: white;
+font-family: Girassol, sans-serif;
+font-size: 3rem;
+`
+
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+`
+
+const Input = styled.input`
+    height: 50px;
+    font-size: 2rem;
+    width: 250px;
+    border-radius: 5px;
+    font-family: Girassol, sans-serif;
+    background-color: rgba(0, 0, 0, 0.6);
+    border: none;
+    color: white;
+    margin: 1rem;
+    text-indent: 20px;
+`
+
+
+export default Login
