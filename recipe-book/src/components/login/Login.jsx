7723a758d45a7bf1a591d98f3f683a823411cd7f
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-04-03 03:04:20 +0300

    Login page is ready

--- recipe-book/src/components/login/Login.jsx
+++ recipe-book/src/components/login/Login.jsx
@@ -8,11 +8,12 @@ function Login() {
         <Section>
             <Image src='https://recipe-app-solution.vercel.app/static/media/meal.9eba888b37c920112c046969b9256a41.svg' />
             <Header>
-            &#60;Burak&#62;"Recipe"
+            &#60;Burak&#62;Recipe
             </Header>
             <Form>
                 <Input type="text" placeholder='username' required/>
                 <Input type = "password" placeholder='password' required/>
+                <Button type='submit'>Login</Button>
             </Form>
             
         </Section>
@@ -79,5 +80,13 @@ const Input = styled.input`
     text-indent: 20px;
 `
 
-
+const Button = styled.button`
+font-size: 2rem;
+    font-family: Girassol, sans-serif;
+    background-color: rgba(0, 0, 0, 0.6);
+    color: white;
+    border: none;
+    cursor: pointer;
+    margin: 1rem;
+`
 export default Login
