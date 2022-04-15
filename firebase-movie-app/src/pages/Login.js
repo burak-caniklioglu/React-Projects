2c07befcb535a59e913b05b29fe62bfa636cdbb6
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-04-15 20:56:08 +0300

    Project is done

--- firebase-movie-app/src/pages/Login.js
+++ firebase-movie-app/src/pages/Login.js
@@ -1,48 +1,76 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { forgotPassword, signIn, signUpProvider } from "../auth/firebase";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setEmail("");
-    setPassword("");
-  }
+    signIn(email, password, navigate);
+    console.log(email, password);
+  };
+
+  const handleProviderLogin = () => {
+    signUpProvider(navigate);
+  };
   return (
     <div className="d-flex justify-content-center">
-    <div className="form-image">
-      <img src={'https://picsum.photos/800/800'} alt=''/>
-    </div>
-    <div className="register-form">
-      <h1 className="form-title display-3">Login</h1>
-      <form id="register" onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">E-mail</label>
-          <input 
-          type="text" 
-          className="form-control"
-          id="email"
-          placeholder="Enter your email address.."
-          onChange={(e) => setEmail(e.target.value)} 
-          required
-          value={email}/>
+      {window.innerWidth > 700 && (
+        <div className="form-image">
+          <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
         </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">password</label>
-          <input 
-          type="text" 
-          className="form-control"
-          id="password"
-          placeholder="Enter your password.." 
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          value={password}/>
-        </div>
-        <input type="submit" className ='btn btn-primary form-control' value="Login" />
-      </form>
-      <button className="btn btn-primary form-control">Continue with Google</button>
+      )}
+      <div className="register-form">
+        <h1 className="form-title display-3">Login</h1>
+        <form id="register" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email adress.."
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password.."
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="link" onClick={() => forgotPassword(email)}>
+            Forgot password?
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Login"
+            // onSubmit={handleSubmit}
+          />
+        </form>
+        <button
+          className="btn btn-primary form-control"
+          onClick={handleProviderLogin}
+        >
+          Continue with Google
+        </button>
+      </div>
     </div>
-  </div>)
+  );
 };
 
 export default Login;
